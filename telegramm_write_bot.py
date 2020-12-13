0e0ed2dcbdeb56e41dfdfcbbdc1ed7ba91b19508
DenchicEz <65415569+denchicez@users.noreply.github.com>
2020-12-13 21:35:31 +0700

    Add files via upload

---
+++ telegramm_write_bot.py
@@ -0,0 +1,6 @@
+import telebot
+def write_to_bot(text):
+    bot=telebot.TeleBot('1487993906:AAEOqag-VZFj1rfYo3zQDEFmcUGspY-zh50')
+    bot.send_message(-1001209168677, text)
+#write_to_bot('Obama')
+#post to https://t.me/sportiviti
