a360720e86747dd36f7fb77a18ecb5a3db857c1d
NAD777 <nehaev07.07.2003@gmail.com>
2020-12-14 12:13:24 +0300

    Different telegram send

--- telegramm_write_bot.py
+++ telegramm_write_bot.py
@@ -1,6 +1,22 @@
 import telebot
-def write_to_bot(text):
-    bot=telebot.TeleBot('1487993906:AAEOqag-VZFj1rfYo3zQDEFmcUGspY-zh50')
-    bot.send_message(-1001209168677, text)
-#write_to_bot('Obama')
-#post to https://t.me/sportiviti
+import requests as rq
+
+#
+# def write_to_bot(text):
+#     bot = telebot.TeleBot('1487993906:AAEOqag-VZFj1rfYo3zQDEFmcUGspY-zh50')
+#     bot.send_message(-1001209168677, text)
+#
+#
+# write_to_bot('Obama')
+
+# # post to https://t.me/sportivit
+
+CHAT_ID = -1001209168677
+TOKEN = "1487993906:AAEOqag-VZFj1rfYo3zQDEFmcUGspY-zh50"
+
+
+def send_message_to_channel(text, chat_id):
+    rq.post(f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={text}")
+
+
+send_message_to_channel("HELLO It's MARIO", CHAT_ID)
