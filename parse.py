bf5e4667b9685447fb06ab0395c79ad03ff4e940
NAD777 <nehaev07.07.2003@gmail.com>
2020-12-14 11:46:19 +0300

    Need tests

--- parse.py
+++ parse.py
@@ -15,7 +15,7 @@ def get_today_matches() -> dict:
         #     f'http://livescore-api.com/api-client/scores/live.json?key={KEY}&secret'
         #     f'={SECRET}&competition_id={num_lig}')
         req = rq.get('http://livescore-api.com/api-client/scores/history.json?key=qACKZM1CUVIaCa3g'
-                    '&secret=GD8GLhMdizlJoGWOgyzfkASfwAq9Ltps&from=2020-12-12&to=2020-12-13')
+                     '&secret=GD8GLhMdizlJoGWOgyzfkASfwAq9Ltps&from=2020-12-12&to=2020-12-13')
         # print(json.loads(req.text))
         data = json.loads(req.text)
         for dat in data['data']['match']:
@@ -23,22 +23,45 @@ def get_today_matches() -> dict:
         return ans_dict
 
 
-def get_match_events(match_id: int) -> dict:
-    req = rq.get(f"http://livescore-api.com/api-client/scores/events.json"
-                 f"?key={KEY}&secret={SECRET}&id="
-                 f"{match_id}&sort=5")
-    return dict(json.loads(req.text))
+class Match:
+    def __init__(self, match_id: int, data: dict):
+        self.match_id = match_id
+        self.counter_event = -1
+        self.stats = {
+            'h': {},  # home command
+            'a': {}  # away command
+        }
+        self.teams_names = {
+            'h': data['home_name'],
+            'a': data['away_name']
+        }
+        self.score = {
+            'h': 0,
+            'a': 0
+        }
+        self.status = data['status']
 
+    def update(self):
+        info = self._get_match_events()
+        events = filter(lambda x: x['sort'] > self.counter_event, info['event'])
+        for el in events:
+            self.stats[el['home_away']][el['player']][el['event']] += 1
+        self.score['h'], self.score['a'] = map(int, info['match']['match_score'].split(' - '))
 
-def get_last_event(match_id: int) -> dict:
-    req = get_match_events(match_id)
-    return dict(req['data']['event'][-1])
+    def _get_match_events(self) -> dict:
+        req = rq.get(f"http://livescore-api.com/api-client/scores/events.json"
+                     f"?key={KEY}&secret={SECRET}&id="
+                     f"{self.match_id}&sort=5")
+        return json.loads(req.text)['data']
 
+    def get_summary(self) -> dict:
+        req = rq.get(f'http://livescore-api.com/api-client/matches/stats.json?match_id='
+                     f'{self.match_id}'
+                     f'&key={KEY}&secret={SECRET}')
+        return dict(json.loads(req.text)['data'])
 
-def get_summary(match_id: int) -> dict:
-    req = rq.get(f'http://livescore-api.com/api-client/matches/stats.json?match_id={match_id}'
-                 f'&key={KEY}&secret={SECRET}')
-    return dict(json.loads(req.text)['data'])
+    def get_player_stats(self, name: str) -> dict:
+        return self.stats['h'][name] if name in self.stats['h'].keys() else self.stats['a'][name]
 
 
 if __name__ == '__main__':
@@ -52,4 +75,4 @@ if __name__ == '__main__':
     # print()
     # pprint(get_last_event(first_id))
     # pprint(get_today())
-    pprint(get_summary(first_id))
+    # pprint(get_summary(first_id))
