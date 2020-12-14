4de8aacae86b7b909b2c95e0c4f283c7c5837e17
NAD777 <nehaev07.07.2003@gmail.com>
2020-12-14 10:10:54 +0300

    hot fix get_summary

--- parse.py
+++ parse.py
@@ -6,46 +6,50 @@ KEY = "qACKZM1CUVIaCa3g"
 SECRET = "GD8GLhMdizlJoGWOgyzfkASfwAq9Ltps"
 
 
-def get_today_matches() -> json:
+def get_today_matches() -> dict:
     # arr = [2, 3, 4, 6, 45]
     arr = [45]
     ans_dict = dict()
     for num_lig in arr:
-        req = rq.get(
-            f'http://livescore-api.com/api-client/scores/live.json?key={KEY}&secret'
-            f'={SECRET}&competition_id={num_lig}')
+        # req = rq.get(
+        #     f'http://livescore-api.com/api-client/scores/live.json?key={KEY}&secret'
+        #     f'={SECRET}&competition_id={num_lig}')
+        req = rq.get('http://livescore-api.com/api-client/scores/history.json?key=qACKZM1CUVIaCa3g'
+                    '&secret=GD8GLhMdizlJoGWOgyzfkASfwAq9Ltps&from=2020-12-12&to=2020-12-13')
         # print(json.loads(req.text))
         data = json.loads(req.text)
         for dat in data['data']['match']:
             ans_dict[dat['id']] = [dat['status'], dat['score'], dat['time']]
-        return json.dumps(ans_dict)
+        return ans_dict
 
 
-def get_match_events(match_id: int) -> json:
+def get_match_events(match_id: int) -> dict:
     req = rq.get(f"http://livescore-api.com/api-client/scores/events.json"
                  f"?key={KEY}&secret={SECRET}&id="
                  f"{match_id}&sort=5")
-    return json.loads(req.text)
+    return dict(json.loads(req.text))
 
 
-def get_last_event(match_id: int) -> json:
+def get_last_event(match_id: int) -> dict:
     req = get_match_events(match_id)
-    return req['data']['event'][-1]
+    return dict(req['data']['event'][-1])
 
 
-def get_summary(match_id: int) -> json:
+def get_summary(match_id: int) -> dict:
     req = rq.get(f'http://livescore-api.com/api-client/matches/stats.json?match_id={match_id}'
                  f'&key={KEY}&secret={SECRET}')
-    return json.loads(req)['data']
+    return dict(json.loads(req.text)['data'])
 
 
 if __name__ == '__main__':
     today = get_today_matches()
-    pprint(today)
-    # first_id = today[0]['id']
+    # pprint(today)
+    first_id = list(today.keys())[0]
+
     # print("ID:", first_id)
     # print()
     # pprint(get_match_events(first_id))
     # print()
     # pprint(get_last_event(first_id))
     # pprint(get_today())
+    pprint(get_summary(first_id))
