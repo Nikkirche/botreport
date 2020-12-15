84b8039439fc1d1ba17e87e3544f46731c2810f6
nikkirche <Slonik2004!>
2020-12-15 15:44:51 +0300

    unfinished generating text for events

---
+++ text_generator_for_events.py
@@ -0,0 +1,110 @@
+import random
+
+import parse
+
+events_with_generated_text = dict()
+score_away = 0
+score_home = 0
+
+
+def get_type_of_event(event):
+    event_type = event.get('event')
+    from_which_side = random.random()
+    if event_type == "GOAL":
+        types = get_types_of_goal(event)
+        i = random.randint(0, len(types) - 1)
+        return generate_text_for_goals(event, types[i], from_which_side)
+    elif event_type == "YELLOW_RED_CARD":
+        types = get_types_of_yellow_red_card(event)
+        i = random.randint(0, len(types) - 1)
+        return generate_text_for_yellow__red_card(event, types[i], from_which_side)
+    elif event_type == "RED_CARD":
+        types = get_types_of_red_card(event)
+        i = random.randint(0, len(types) - 1)
+        return generate_text_for_red_card(event, types[i], from_which_side)
+    elif event_type == "YELLOW_CARD":
+        types = get_types_of_yellow_card(event)
+        i = random.randint(0, len(types) - 1)
+        return generate_text_for_yellow_card(event, types[i], from_which_side)
+    elif event_type == "OWN_GOAL":
+        types = get_types_of_own_goal(event)
+        i = random.randint(0, len(types) - 1)
+        return generate_text_for_own_goal(event, types[i], from_which_side)
+    elif event_type == "GOAL_PENALTY":
+        types = get_types_of_penalty_goals(event)
+        i = random.randint(0, len(types) - 1)
+        return generate_text_for_penalty_goal(event, types[i], from_which_side)
+
+
+def get_types_of_goal(event):
+    types = []
+    if ():
+        types.append("DEFEATING")
+    else:
+        types.append("BASE1")
+        types.append("BASE2")
+        types.append("BASE3")
+        types.append("BASE4")
+    return types
+
+
+def get_types_of_own_goal(event):
+    types = []
+    return types
+
+
+def get_types_of_penalty_goals(event):
+    types = []
+    return types
+
+
+def get_types_of_yellow_card(event):
+    types = []
+    return types
+
+
+def get_types_of_yellow_red_card(event):
+    types = []
+    return types
+
+
+def get_types_of_red_card(event):
+    types = []
+    return types
+
+
+def generate_text_for_penalty_goal(event, types, from_which_side):
+    return "test"
+
+
+def generate_text_for_own_goal(event, types, from_which_side):
+    return "test"
+
+
+def generate_text_for_red_card(event, types, from_which_side):
+    return "test"
+
+
+def generate_text_for_yellow__red_card(event, types, from_which_side):
+    return "test"
+
+
+def generate_text_for_yellow_card(event, types, from_which_side):
+    return "test"
+
+
+def generate_text_for_goals(event, types, from_which_side):
+    return "test"
+
+
+dct = parse.Controller().get_today_matches()
+f_id = list(dct.keys())[0]
+dat = list(dct.values())[0]
+match = parse.Match(f_id, dat)
+match.update()
+score_home = match.score_home
+score_away = match.score_away
+events = match.get_events()
+print(events)
+for i in events:
+    print(get_type_of_event(i))
---
+++ text_generator_for_results.py
@@ -0,0 +1,66 @@
+import random
+
+
+def get_types_of_game(goals_1, goals_2, team1, team2):
+    types = []
+    if goals_1 < goals_2:
+        goals_1, goals_2 = goals_2, goals_1
+        team1, team2 = team1, team2
+    if goals_1 == goals_2:
+        print()
+    elif (goals_1 - goals_2) > 0:
+        if goals_2 == 0:
+            types.append("unanswered")
+        if (goals_1 - goals_2) > 2:
+            types.append("smashing")
+    return types
+
+
+def generate_text_for_wins(game_type, winner, loser):
+    # 0 for loser ,1 for winner
+    from_which_side = random.random()
+    if game_type == "unanswered":
+        if from_which_side:
+            return f"{winner} scored five unanswered goals in the gate {loser}"
+        else:
+            return f"Five unanswered goals scored to {loser}"
+
+    elif game_type == "smashed":
+        if from_which_side:
+            return f"the {winner} defeated the {loser}"
+        else:
+            return f" {loser} was defeated"
+
+
+def generate_text_for_drafts(game_type):
+    from_which_side = random.random()
+
+
+def find_loser_and_winner(goals_team_1, goals_team_2, team_1, team_2):
+    if goals_team_2 > goals_team_1:
+        winner = team_2
+        loser = team_1
+    else:
+        winner = team_2
+        loser = team_1
+    return winner, loser
+
+
+def is_draw(goals_1, goals_2):
+    return goals_1 == goals_2
+
+
+def main():
+    # Sample Data
+    score = "1:0"
+    team_1 = "barnaul"
+    team_2 = "barcelona"
+    goals_team_1 = int(score[0])
+    goals_team_2 = int(score[2])
+    types = get_types_of_game(goals_team_1, goals_team_2,team_1,team_2)
+    i = random.randint(0, len(types) - 1)
+    if not is_draw(goals_team_1, goals_team_2):
+        roles = find_loser_and_winner(goals_team_1, goals_team_2, team_1, team_2)
+        print(generate_text_for_wins(types[i], roles[0], roles[1]))
+    else:
+        print(generate_text_for_drafts(types[i]))
