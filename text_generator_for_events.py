33e83caf331fb1aa85731c7ba08e72d0cfa496a7
nikkirche <Slonik2004!>
2020-12-17 15:56:36 +0300

    upd

--- text_generator_for_events.py
+++ text_generator_for_events.py
@@ -22,26 +22,26 @@ class Event():
             self.score_active = data.get("score_away")
             self.score_passive = data.get("score_home")
 
-    def format_text(self, event, patterns):
-        objects = dict(player=event.player, time=event.time, id_in_match=event.id_in_match,
-                       active=event.active, passive=event.passive,
-                       score_active=event.score_active, score_passive=event.score_passive)
-        if event.type == "YELLOW_CARD":
+    def format_text(self, patterns):
+        objects = dict(player=self.player, time=self.time, id_in_match=self.id_in_match,
+                       active=self.active, passive=self.passive,
+                       score_active=self.score_active, score_passive=self.score_passive)
+        if self.type == "YELLOW_CARD":
             i = random.randint(0, len(patterns["YELLOW_CARD"]) - 1)
             text = patterns["YELLOW_CARD"][i].format(**objects)
-        elif event.type == "YELLOW_RED_CARD":
+        elif self.type == "YELLOW_RED_CARD":
             i = random.randint(0, len(patterns["YELLOW_RED_CARD"]) - 1)
             text = patterns["YELLOW_RED_CARD"][i].format(**objects)
-        elif event.type == "RED_CARD":
+        elif self.type == "RED_CARD":
             i = random.randint(0, len(patterns["RED_CARD"]) - 1)
             text = patterns["RED_CARD"][i].format(**objects)
-        elif event.type == "GOAL":
+        elif self.type == "GOAL":
             i = random.randint(0, len(patterns["GOAL"]) - 1)
             text = patterns["GOAL"][i].format(**objects)
-        elif event.type == "OWN_GOAL":
+        elif self.type == "OWN_GOAL":
             i = random.randint(0, len(patterns["OWN_GOAL"]) - 1)
             text = patterns["OWN_GOAL"][i].format(**objects)
-        elif event.type == "GOAL_PENALTY":
+        elif self.type == "GOAL_PENALTY":
             i = random.randint(0, len(patterns["GOAL_PENALTY"]) - 1)
             text = patterns["GOAL_PENALTY"][i].format(**objects)
         return text
@@ -52,31 +52,27 @@ def generate():
     match = controller[0]
     match.update()
     events = match.get_events()
-    print(events)
     patterns = read_patterns()
     for i in events:
         event = Event(i)
-        print(event.format_text(event, patterns))
+        print(event.format_text(patterns))
 
 
 def read_patterns():
     file = open("patterns.txt")
-    patterns = {"YELLOW_CARD":[],"YELLOW_RED_CARD":[],"RED_CARD":[],"GOAL":[],"OWN_GOAL":[],"GOAL_PENALTY":[]}
+    patterns = {"YELLOW_CARD": [], "YELLOW_RED_CARD": [], "RED_CARD": [], "GOAL": [], "OWN_GOAL": [],
+                "GOAL_PENALTY": []}
     read_started = False
     for line in file:
-        if   not read_started and line == "start\n":
-            read_started= True
-        elif  read_started and line[:6] == "type =":
-            current = line[8:len(line)-2]
+        if not read_started and line == "start\n":
+            read_started = True
+        elif read_started and line[:6] == "type =":
+            current = line[8:len(line) - 2]
         elif line == "exit":
             file.close()
             break
-        elif line[0] =="#":
+        elif line[0] == "#":
             pass
         else:
             patterns[current].append(line.rstrip())
-    print(patterns)
     return patterns
-
-
-generate()
