4f35fcb3f8d623e8709ae71073c140397ae810dd
DenchicEz <65415569+denchicez@users.noreply.github.com>
2020-12-19 19:55:37 +0700

    Update telegramm_write_bot.py

--- telegramm_write_bot.py
+++ telegramm_write_bot.py
@@ -1,5 +1,5 @@
 #!pip3 install PyTelegramBotAPI
-# !pip3 install gtts
+#!pip3 install gtts
 import telebot
 import random
 from gtts import gTTS
@@ -11,11 +11,20 @@ bot = telebot.TeleBot(TOKEN)
 
 
 def send_message_to_channel(type_event, text, chat_id=CHAT_ID):
-    language = 'en'
-    # speech = gTTS(text=text, lang='en', slow=False)
-    # speech.save("voice.mp3")
-    bot.send_audio(chat_id, open("voice.mp3", 'rb'))
     bot.send_message(chat_id, text)
+    new_text=''
+    for char in text:
+        try:
+            if(new_text[-1]!=char):
+                new_text=new_text+char
+        except:
+            new_text=new_text+char
+    new_text.replace('!','')
+    new_text.replace(',','')
+    new_text.replace('.','')
+    speech = gTTS(text=new_text, lang='en', slow=False)
+    speech.save("voice.mp3")
+    bot.send_audio(chat_id, open("voice.mp3", 'rb'))
     rnd = random.randint(1, 100)
     if rnd >= 5 and rnd <= 9:
         if type_event == 'GOAL':
@@ -23,5 +32,4 @@ def send_message_to_channel(type_event, text, chat_id=CHAT_ID):
         if type_event == 'PENALTI':
             bot.send_sticker(CHAT_ID, 'CAACAgIAAxkBAAEBrxFf2KKkfzHy5iLRY_XCTiSttfxYzAAC_wADVp29Ctqt-n3kvEAkHgQ')
 
-
-# send_message_to_channel('GOAL', 'GOAAAAAAAAAAAAAAAAAAL', CHAT_ID)
+#send_message_to_channel('GOAL', 'GOAAAAAAAAAAAAAAAAAAL', CHAT_ID)
